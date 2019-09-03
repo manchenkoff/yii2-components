4e6a0c414e5bfce7b7f9f70b709fae7c9542b6b9
Artem Manchenkov <artyom@manchenkoff.me>
2019-09-03 19:16:12 +0300

    SafeModel trait added

---
+++ src/database/traits/SafeModel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by Artyom Manchenkov
+ * artyom@manchenkoff.me
+ * manchenkoff.me © 2019
+ */
+
+namespace manchenkov\yii\database\traits;
+
+use yii\base\Model;
+
+/**
+ * Trait SafeModel allows loading Models without 'formName' property
+ *
+ * @mixin Model
+ */
+trait SafeModel
+{
+    /**
+     * @inheritDoc
+     */
+    public function formName()
+    {
+        return '';
+    }
+}
